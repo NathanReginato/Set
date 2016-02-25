2c861bf564007992b1c63b54d0d36cb81e72df9f
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-25 09:59:39 -0700

    Finished getting svgs ready. Decided to use inline svgs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var cards = [[0,0,0,0], [0,0,0,1], [0,0,0,2], [0,0,0,1], [0,0,0,1], [1,0,0,0]]
+var cards = [[2,2,2,2], [0,0,0,1], [0,0,0,2], [0,0,0,1], [0,0,0,1], [1,0,0,0]]
+
+// getCard = function(card) {
+//   var number = card[0]
+//   var shape = card[1]
+//   var shading = card[2]
+//   var color = card[3]
+//
+//   makeCard = function() {
+//     var cardToAppend = ''
+//
+//     if (number === 0) {
+//
+//     }
+//   }
+// }
+
+alert("Anything");
