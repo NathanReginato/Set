6c6c855c8540451033d8211074a000d5aaff4eac
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-29 13:20:46 -0700

    Starting twitter api

--- app/js/playerInteraction.js
+++ app/js/playerInteraction.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+  $('.start-game').append('<div class="start-body"><div class="new-game">New Game</div><div class="tutorial">Tutorial</div><div class="about">About</div></div>');
+
+  $('.new-game').on('click', function() {
+    $('.start-game').empty()
+    $('.start-game').append('<div class="form-body"><form class="the-form"><fieldset><input class="pname" type="text"><input type="submit" value="Submit"></fieldset></form></div>')
+  });
   // newGame()
-})
+});
