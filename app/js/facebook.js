6dbe45ce5bbaa61e4bc543b8ddecc7d028bbc4e0
Nathan Reginato <nathan.reginato@gmail.com>
2016-03-02 15:45:31 -0700

    Facebook shows on click

--- app/js/facebook.js
+++ app/js/facebook.js
@@ -3,23 +3,30 @@ console.log("facebook");
 $.ajaxSetup({
   cache: true
 });
+
+function updateStatusCallback() {
+  alert('Status updated!!');
+}
+
 $.getScript('//connect.facebook.net/en_US/sdk.js', function() {
   FB.init({
-    appId: '{1710219289192635}',
+    appId: '1710219289192635',
     version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
   });
   $('#loginbutton,#feedbutton').removeAttr('disabled');
   FB.getLoginStatus(updateStatusCallback);
-});
 
-//
-// FB.ui({
-//   method: 'share_open_graph',
-//   action_type: 'og.likes',
-//   action_properties: JSON.stringify({
-//     object:'https://developers.facebook.com/docs/',
-//   })
-// }, function(response){
-//   // Debug response (optional)
-//   console.log(response);
-// });
+$(document).delegate('.share', 'click', function() {
+    console.log('clicked');
+    FB.ui({
+      method: 'share_open_graph',
+      action_type: 'og.likes',
+      action_properties: JSON.stringify({
+        object: 'https://developers.facebook.com/docs/',
+      })
+    }, function(response) {
+      // Debug response (optional)
+      console.log(response);
+    });
+  })
+});
